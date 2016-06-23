d197240c9f7654b40fb17526e952c4b68215cb86
CCSCEO <bobbycole117@gmail.com>
2016-06-23 13:27:01 -0500

    TestUpdate
    test

--- src/InternalH2.java
+++ src/InternalH2.java
@@ -127,15 +127,12 @@ public class InternalH2 implements InternalDB {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return f;
 	}
-
 
 	public File toXML(ResultSet rs) throws SQLException, FileNotFoundException{
 		new File("./results/").mkdirs();
 		File f = new File("./results/"+rs.getMetaData().getTableName(1) + ".xml");
-		
 		FileOutputStream fos = new FileOutputStream(f);
 		OracleWebRowSet set = new OracleWebRowSet();
 				set.writeXml(rs, fos);
