7ae56bb3e61de31348dff8323c684b48e8c20c3b
Caleb Kim <dream4ever012@gmail.com>
2016-06-30 11:31:53 -0500

    MINOR

--- src/ClassNameTableData.java
+++ src/ClassNameTableData.java
@@ -20,7 +20,7 @@ public class ClassNameTableData {
 				"'PCA14','\"If a drug library’s soft limit is violated by the proposed settings, but not a hard limit, a warning is shown to the clinician by the user interface and a distinctive, irritating sound made and recorded in the Event Log.\"','Hotmail','4/14/2016','V2.5'",
 				"'PCA15','\"The PCA pump shall implement a safety architecture112 that separates normal operation from fault detection and response.\"','Hotmail','4/15/2016','V3.5'",
 				"'PCA16','\"When the stop button is pressed, the current pump stroke shall be completed prior to stopping the pump.\"','Hotmail','4/16/2016','V1.6'",
-				"'PCA17','\"A open door alarm shall be triggered when the reservoir door is opened while the pump is not stopped.\"','Jane','4/17/2016','V3.1'",
+				"'PCA17','\"A open door alarm shall be triggered when the reservoir door is opened while the pump is not stopped.\"','Jane','4/17/2016','V2.6'",
 				"'PCA18','\"The PCA pump shall continue to infuse for 10 minutes during interruption of mains electricity supply using battery backup, either continuously or spread over an hour. (Five minutes to recharge per minute using battery.)\"','Jane','4/18/2016','V3.6'",
 				"'PCA19','Component failure must not harm patient (beyond stopping function).','Jane','4/19/2016','V1.7'",
 				"'PCA20','The PCA pump must be electromagnetically compatible according to IEC 60601-1-2 (2001)','Jane','4/20/2016','V2.7'",
@@ -31,5 +31,4 @@ public class ClassNameTableData {
 				"'PCA25','\"Drug container must have a valid prescription, filled by an authorized pharmacist, for the particular patient to be infused by the PCA pump.\"','Sugandah','4/25/2016','V1.9'",
 				"'PCA26','\"Provisioning shall be a single, unitary block-transfer.\"','Sugandah','4/26/2016','V2.9'"				
 		};
-	
 }
