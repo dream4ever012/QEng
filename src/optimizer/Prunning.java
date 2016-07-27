c05048fc8cf652c804e6b22b55a008cbbb9ff392
Caleb Kim <dream4ever012@gmail.com>
2016-07-27 16:29:20 -0500

    queryScheme2

---
+++ src/optimizer/Prunning.java
@@ -0,0 +1,17 @@
+package optimizer;
+
+import java.io.File;
+
+import qEng.InternalDB;
+
+public class Prunning implements Query{
+
+	@Override
+	public File ExecuteQuery(String SQL, InternalDB internalDBImpl) {
+		// TODO Auto-generated method stub
+		// prunning unecessary columns and rows at each join operation
+		
+		return null;
+	}
+
+}
