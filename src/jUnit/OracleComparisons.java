ed7069eaa1dc4631f50a176e6ec6a630e84f283d
CCSCEO <bobbycole117@gmail.com>
2016-07-27 13:35:39 -0500

    Setting up for oracle comparisons
    ^

---
+++ src/jUnit/OracleComparisons.java
@@ -0,0 +1,14 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OracleComparisons {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
