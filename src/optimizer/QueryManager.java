2d163cc3b4da823546bef2fd6fdcddc23d8b2850
Caleb Kim <dream4ever012@gmail.com>
2016-07-28 08:26:54 -0500

    QueryScheme4.1

--- src/optimizer/QueryManager.java
+++ src/optimizer/QueryManager.java
@@ -11,3 +11,23 @@ public interface QueryManager {
 	boolean importCSVAsTable(String file, String tableName);
 	
 }
+
+/*
+ *
+ * 
+ * in terms of optimization scheme, factory pattern seems to be a great idea
+3*2*2*2 strategies
+but, in terms of pruning and skipping
+do such must perform better 
+yet would like to know what the benefit would be
+before making up such strategy
+what is pre-required beforehand is table and UDF stats
+scancost is not hard to do that you select column from table and divided with the number of rows and then take an average cost per 10k rows or sth like that
+cpu cost maybe a method, you read the first row of a columns and then select * from table where col.a = col.1st_row
+then this would be one time scancost and the cpu cost 
+cpu cost would be the minimal
+ * 
+ * 
+ * 
+ * 
+*/
