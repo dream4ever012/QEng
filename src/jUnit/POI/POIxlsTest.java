a5f2343d1dd84774964dc9ec3435fe480ec426b2
Caleb Kim <dream4ever012@gmail.com>
2016-08-04 14:03:25 -0500

    mockdata finalized

--- src/jUnit/POI/POIxlsTest.java
+++ src/jUnit/POI/POIxlsTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import qEng.InternalDB;
 import qEng.InternalH2;
+import utils.MeasureCostToRS;
 import utils.POI.SheetReader;
 
 public class POIxlsTest {
@@ -30,6 +31,39 @@ public class POIxlsTest {
 		private static final String CCTableName = "codeclass";
 		private static final String CCSheetFP = "./SecondData/codeclasses.xls";
 		private static final String TMTableName = "CC_REQ_TM";
+		
+		private static final String CC10kFP = "./SecondData/CC.xls";
+		private static final String CC10kTableName = "CC";
+		private static final String CC_UCS16kFP = "./SecondData/CCaaUCS.xls";
+		private static final String CC_UCS16kTableName = "CCaaUCS";
+		private static final String CC_SCP12kFP = "./SecondData/CCaaSCP.xls";
+		private static final String CC_SCP12kTableName = "CCaaSCP";
+		private static final String G70FP = "./SecondData/G.xls";
+		private static final String G70TableName = "G";
+		private static final String G_UC8kFP = "./SecondData/GaaUC.xls";
+		private static final String G_UC8kTableName = "GaaUC";
+		private static final String UC_UCS15kFP = "./SecondData/UCaaUCS.xls";
+		private static final String UC_UCS15kTableName = "UCaaUCS";
+		private static final String UCS20kFP = "./SecondData/UCS.xls";
+		private static final String UCS20kTableName = "UCS";
+		private static final String UCS_EC16kFP = "./SecondData/UCSaaEC.xls";
+		private static final String UCS_EC16kTableName = "UCSaaEC";
+		private static final String EC10kFP = "./SecondData/EC.xls";
+		private static final String EC10kTableName = "EC";
+		private static final String EC_ECS24kFP = "./SecondData/ECaaECS.xls";
+		private static final String EC_ECS24kTableName = "ECaaECS";
+		private static final String ECS30kFP = "./SecondData/ECS.xls";
+		private static final String ECS30kTableName = "ECS";
+		
+		private static final String R70FP = "./SecondData/RQ.xls";
+		private static final String R70TableName = "RQ";
+		private static final String RQ_CP7kFP = "./SecondData/RQaaCP.xls";
+		private static final String RQ_CP7kTableName = "RQaaCP";
+		private static final String CP10kFP = "./SecondData/CP.xls";
+		private static final String CP10kTableName = "CP";
+		private static final String CP_SCP12kFP = "./SecondData/CPaaSCP.xls";
+		private static final String CP_SCP12kTableName = "CPaaSCP";
+
 
 		private static final String ResultsURL = "./Results/POIxlsTest/";
 		@Before
@@ -50,12 +84,59 @@ public class POIxlsTest {
 			myDB.ImportSheet(REQSheetFP,REQTableName);
 			myDB.ImportSheet(CCSheetFP,CCTableName);
 			
+			myDB.ImportSheet(CC10kFP, CC10kTableName);
+			myDB.ImportSheet(CC_UCS16kFP, CC_UCS16kTableName);
+		
+			
+			myDB.ImportSheet(CC_SCP12kFP, CC_SCP12kTableName);
+			myDB.ImportSheet(G70FP, G70TableName);
+			myDB.ImportSheet(G_UC8kFP, G_UC8kTableName);
+			myDB.ImportSheet(UC_UCS15kFP, UC_UCS15kTableName);
+			myDB.ImportSheet(UCS20kFP, UCS20kTableName);
+			myDB.ImportSheet(UCS_EC16kFP, UCS_EC16kTableName);
+			myDB.ImportSheet(EC10kFP, EC10kTableName);
+			myDB.ImportSheet(EC_ECS24kFP, EC_ECS24kTableName);
+			myDB.ImportSheet(ECS30kFP, ECS30kTableName);
+			myDB.ImportSheet(R70FP, R70TableName);
+			myDB.ImportSheet(RQ_CP7kFP, RQ_CP7kTableName);
+			myDB.ImportSheet(CP10kFP, CP10kTableName);
+			myDB.ImportSheet(CP_SCP12kFP, CP_SCP12kTableName);
+			/* */
+
+			
 			//read CSV trace matrix
 			String ArbSQL = "DROP TABLE "+ TMTableName +" IF EXISTS; CREATE TABLE "+ TMTableName +" AS SELECT * FROM CSVREAD('./Data/CC-REQ-TM.csv');";
 			myDB.arbitrarySQL(ArbSQL);
 			setupIsDone = true;
 			}
 		}
+		
+		@Test
+		public void JoinMockData() {
+			File TQ1 = new File(ResultsURL + "TQ1.xml");
+
+			SQLString = "SELECT " + CC10kTableName + ".classname " + // REQTableName + ".* " + 
+					"FROM " + CC10kTableName + ";";// " " + 
+					//"INNER JOIN " + REQTableName + " " +
+					//"ON "+ TMTableName + ".ID = "+ REQTableName + ".ID;";
+			
+			// myDB.QueryToXML(SQLString, TQ1);
+			MeasureCostToRS.measureCostToRS(myDB, SQLString, TQ1);
+		}
+		
+		/*
+		@Test
+		public void SimpleJoin21() {
+			File SimpleJoin1 = new File(ResultsURL + "REQandTM2.xml");
+
+			SQLString = "SELECT " + TMTableName + ".ID, " + REQTableName + ".* " + 
+					"FROM " + TMTableName + " " + 
+					"INNER JOIN " + REQTableName + " " +
+					"ON "+ TMTableName + ".ID = "+ REQTableName + ".ID;";
+
+			myDB.QueryToXML(SQLString, SimpleJoin1);
+		}
+
 		
 		@Test
 		public void SimpleJoin1() {
@@ -136,4 +217,5 @@ public class POIxlsTest {
 			myDB.QueryToXML(SQLString, TripleJoin);
 
 		}
+		*/
 	}
